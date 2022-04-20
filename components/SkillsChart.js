23a7714ed3103a4e4e4a5d2f96bfeb0bca4162a4
jacpolanski <jac.polanski@gmail.com>
2022-04-20 21:37:01 +0200

    Added basic skill-chart

---
+++ components/SkillsChart.js
@@ -0,0 +1,71 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  ArcElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { PolarArea } from "react-chartjs-2";
+
+ChartJS.register(RadialLinearScale, ArcElement, Tooltip, Legend);
+
+const generateRGBA = (a) => {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  let rgba = "rgb(" + r + "," + g + "," + b + "," + a + ")";
+  return rgba;
+};
+
+const labels = [
+  "Organizational skills",
+  "Self-development",
+  "Attention to detail",
+  "Friendliness",
+  "Communication skills",
+  "Curiosity",
+];
+
+const skillsData = [73, 70, 60, 59, 61, 81];
+const backgroundColor = labels.map(() => generateRGBA(0.8));
+
+export const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: "# of Importance",
+      data: skillsData,
+      backgroundColor: backgroundColor,
+      borderWidth: 2,
+    },
+  ],
+};
+
+const options = {
+  plugins: {
+    legend: {
+      labels: {
+        color: "white",
+        font: {
+          size: 18,
+        },
+      },
+    },
+  },
+};
+
+function SkillsChart() {
+  return (
+    <Container className="d-flex justify-content-center align-items-center h-100">
+      <PolarArea
+        data={data}
+        options={options}
+        style={{ maxWidth: "50vw", maxHeight: "50vh" }}
+      />
+    </Container>
+  );
+}
+
+export default SkillsChart;
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,34 @@
 import React, { useRef } from "react";
 import HomePage from "../components/HomePage";
+import { Element } from "react-scroll";
+import SkillsChart from "../components/SkillsChart";
 
 export default function Home() {
   return (
-    <>
-      <div className="background-home" id="home">
-        <div className="wrapper-home">
+    <div className="scroll-container">
+      <div className="background-home scroll-snap">
+        <Element className="wrapper-home" name="home">
           <HomePage />
-        </div>
+        </Element>
       </div>
 
-      <div className="background-skills" id="skills">
-        <div className="wrapper-skills"></div>
+      <div className="background-skills">
+        <Element className="wrapper-skills" name="skills">
+          <SkillsChart className="skill-chart" />
+        </Element>
       </div>
 
-      <div className="background-projects" id="projects">
-        <div className="wrapper-projects"></div>
+      <div className="background-projects">
+        <Element className="wrapper-projects" name="projects"></Element>
       </div>
 
-      <div className="background-about-me" id="about-me">
-        <div className="wrapper-about-me"></div>
+      <div className="background-about-me">
+        <Element className="wrapper-about-me" name="about-me"></Element>
       </div>
 
-      <div className="background-contact" id="contact">
-        <div className="wrapper-contact"></div>
+      <div className="background-contact">
+        <Element className="wrapper-contact" name="contact"></Element>
       </div>
-    </>
+    </div>
   );
 }
